a81be7a318dea6151fe43b181511e995dd8bc33d
Priyanka Pandey <priyanka.pandey@modaoperandi.com>
2017-06-05 14:58:04 +0530

    Initial commit

---
+++ app/controllers/manage_data_controller.rb
@@ -0,0 +1,36 @@
+class ManageDataController < ApplicationController
+
+	def create
+		@rooms = Array.new(100)
+        
+	 	File.foreach( params[:uploaded_data].path ).with_index do |line, index|  
+	 		puts line
+	 		lines_to_process = line.to_i if index ==0
+            next if index==0
+	 		visitor_no, room_no, inout, time = line.split.map{|s| /^[0-9]+$/.match(s) ? s.to_i : s}
+            @rooms[room_no] = [] if @rooms[room_no].blank?
+            visited_list = @rooms[room_no].collect{|s| s.keys}.flatten.include?("#{visitor_no}")
+
+            last_visit_data = @rooms[room_no].select{|s| s["#{visitor_no}"]}.first
+            if visited_list
+            	if inout =="I"
+ 					time_spent = last_visit_data["#{visitor_no}"] - time
+ 				else
+ 					time_spent = time - last_visit_data["#{visitor_no}"]
+ 				end 
+ 				last_visit_data["#{visitor_no}"]=time_spent
+            else
+            	@rooms[room_no] << {"#{visitor_no}" => time}
+
+            end
+
+	    end
+
+	    @rooms.each_with_index { |v, i|
+ 		 	next if v.nil? # no one entered the room
+  			total_time = v.map(&:values).flatten.reduce(:+)
+  			puts "Room #{i}, #{total_time/v.size} minute average visit, #{v.size} visitor(s) total"
+		}
+	end
+
+end
